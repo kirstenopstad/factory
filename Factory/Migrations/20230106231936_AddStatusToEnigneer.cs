27b08ff2a68c10f2af523dbe1203e5b1ad56a6f2
Kirsten Opstad <kirsten.opstad@gmail.com>
2023-01-06 15:25:06 -0800

    add migration to add status to Engineer

---
+++ Factory/Migrations/20230106231936_AddStatusToEnigneer.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Factory.Migrations
+{
+    public partial class AddStatusToEnigneer : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Status",
+                table: "Engineers",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Engineers");
+        }
+    }
+}
