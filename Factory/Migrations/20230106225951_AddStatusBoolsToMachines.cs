2954bac43698d6cd72bf4fd471ccaf9f2e91e944
Kirsten Opstad <kirsten.opstad@gmail.com>
2023-01-06 15:15:04 -0800

    add ops and repair status to Machine

---
+++ Factory/Migrations/20230106225951_AddStatusBoolsToMachines.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Factory.Migrations
+{
+    public partial class AddStatusBoolsToMachines : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "OperationalStatus",
+                table: "Machines",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "RepairStatus",
+                table: "Machines",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OperationalStatus",
+                table: "Machines");
+
+            migrationBuilder.DropColumn(
+                name: "RepairStatus",
+                table: "Machines");
+        }
+    }
+}
