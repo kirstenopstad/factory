08c8d4d1ad5669b6b49afe454e3e238f025dcbc2
Kirsten Opstad <kirsten.opstad@gmail.com>
2023-01-06 16:07:05 -0800

    add migration to update database with machine inspection date column

---
+++ Factory/Migrations/20230107000513_AddInspectionDateToMachine.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Factory.Migrations
+{
+    public partial class AddInspectionDateToMachine : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "InspectionDate",
+                table: "Machines",
+                type: "datetime(6)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "InspectionDate",
+                table: "Machines");
+        }
+    }
+}
